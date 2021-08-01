46e776e647d35eb01a90ce00a0ea51e006aebda4
시혁 박 <exon.indiv@gmail.com>
2021-08-01 21:24:26 +0900

    Init:

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+import { Client } from 'discord.js'
+import { readJSONSync } from 'fs-extra'
+
+const PATH = path.resolve()
+const { bot_token : token } = readJSONSync(PATH + '/settings.json')
+export const client = new Client()
+
+client.on('ready', async () => {
+    console.log('[*] Ready')
+     
+})
+
+client.login(token)
