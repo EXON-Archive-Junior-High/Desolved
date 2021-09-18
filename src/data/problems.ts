f5d05e4d6675c1d5ed6cdd333dcaaead322498a8
시혁 박 <exon.indiv@gmail.com>
2021-09-18 21:38:38 +0900

    Data: b3, b2

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -3,7 +3,6 @@ import fetch from 'node-fetch'
 
 const get = async (str: string) => await (await fetch(str)).json()
 
-
 export default async function getProblemsByLevel(level: string) {
     let i = 1
     while (true) {
@@ -11,9 +10,9 @@ export default async function getProblemsByLevel(level: string) {
         problems.items.forEach(async (problem: any) => {
             fs.appendFileSync('./data.txt', `\t\t\t"${problem.problemId}",\n`)
         })
-        if (!problems.items) break
+        if (!problems.items.length) break
         i++
     }
 }
 
-getProblemsByLevel('b4')
+getProblemsByLevel('b1')
